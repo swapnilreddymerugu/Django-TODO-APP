48047d34d5411e4620c1fc806536ac71d7c3f71a
Swapnil Reddy Merugu <swapnilreddymerugu@Swapnils-MacBook-Air.local>
2026-01-29 01:14:35 +0530

    Add Django TODO app source code

---
+++ Todo_main/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from todo.models import Task
+
+
+
+def home(request):
+    tasks=Task.objects.filter(is_completed=False)
+    completed_tasks=Task.objects.filter(is_completed=True)
+    context={
+        'tasks':tasks,
+        'completed_tasks':completed_tasks
+    }
+    return render(request,'home.html',context)
+
